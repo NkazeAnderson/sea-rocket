74d7237a4dfc5af25b6667267b3b1c8b0d6bc3ad
nkazeanderson <nkazeanderson@gmail.com>
2024-05-06 02:41:14 -0700

    Improved Seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,22 @@ import Navbar from "./components/navbar/navbar";
 import Footer from "./components/navbar/footer";
 import Link from "next/link";
 import Image from "next/image";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Sea Rocket Tech Enterprise",
-  description: "Top rated software development company in Cameroon",
+  description:
+    "Sea Rocket Tech Enterprise is a top rated web development company in Cameroon offering advanced web development solutions for mobile, web and cloud",
+  keywords:
+    "Sea Rocket Tech Enterprise, build a website in cameroon,créer un site internet au Cameroun, web developer in cameroon, react developer in cameroon, mobile app developer in cameroon Africa",
+
+  manifest: "/site.webmanifest",
+  icons: {
+    apple: "/apple-touch-icon.png",
+    icon: "/favicon-32x32.png",
+  },
 };
 
 export default function RootLayout({
@@ -20,6 +30,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body className={inter.className}>
         <Navbar />
         <main className="pt-[100px] md:pt-[125px] overflow-y-auto h-screen w-screen overflow-x-hidden">
